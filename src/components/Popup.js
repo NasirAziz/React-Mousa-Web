988027df3df327f9f92de1028e7f22c860b9e35d
haseeb4091 <jasialnew1@gmail.com>
2022-08-22 11:34:55 +0500

    changes

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -18,23 +18,13 @@ function Popup({ setOpenModal }) {
           <h1>Are You Sure You Want to Continue?</h1>
         </div> */}
         <iframe src="https://app.acuityscheduling.com/schedule.php?owner=11325084&action=appt&id%5B%5D=96807e46fd2c98bda3f667ea1df2bfa2
-"></iframe>
+"width="500" height="500" ></iframe>
         <div>
         </div>
         {/* <div className="body">
           <p>The next page looks amazing. Hope you want to go there!</p>
         </div> */}
-        <div className="footer">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            id="cancelBtn"
-          >
-            Cancel
-          </button>
-          <button>Continue</button>
-        </div>
+       
       </div>
     </div>
   );
